b3e7da1ca718ec6f5c8d55b4692a4dcbdfd24cfd
diablo <diablo@patrick-stewart.(none)>
2014-10-02 21:06:04 -0500

    Upgrades convert

--- bin/convert.sh
+++ bin/convert.sh
@@ -3,7 +3,7 @@
 #for i in `find . -type f -name \*.avi -print0 | xargs -0 echo`
 find . -type f -name \*.avi -print0 | while read -d $'\0' file
 do
-avidemux2_cli --video-codec copy --audio-codec AAC --autoindex --force-unpack --load "$file" --output-format MKV --save "${file%????}.mkv"
+avidemux3_cli --video-codec copy --audio-codec AAC --autoindex --load "$file" --output-format MKV --save "${file%????}.mkv"
 if [ $? -eq 0 ]; then
     rm "$file"
 else
