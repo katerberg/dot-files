d6deb23ac68fcaa67a03f190a99f468c1fc46083
Katerberg <katerberg@fastmail.fm>
2015-01-05 21:50:16 -0600

    Manipulates menu

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install tmux conky aptitude git firefox pidgin keepassx claws-mail claws-mail-pgpmime gnupg2 arandr vlc gimp tint2 openbox thunar openbox deluge gnumeric abiword
+sudo apt-get install tmux conky aptitude git firefox pidgin keepassx claws-mail claws-mail-pgpmime gnupg2 arandr vlc gimp tint2 openbox thunar openbox deluge gnumeric abiword rdesktop xchat evince geany
 exit 0
