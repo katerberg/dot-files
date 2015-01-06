fcfd110c2a0084383fad85753260804e033b0aa6
Katerberg <katerberg@fastmail.fm>
2015-01-05 23:41:36 -0600

    claws plugins and screenshots owncloud

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install tmux conky aptitude git firefox pidgin keepassx claws-mail claws-mail-pgpmime gnupg2 arandr vlc gimp tint2 openbox thunar openbox deluge gnumeric abiword rdesktop xchat evince geany
+sudo apt-get install tmux conky aptitude git firefox pidgin keepassx claws-mail claws-mail-pgpmime claws-mail-plugins claws-mail-extra-plugins gnupg2 arandr vlc gimp tint2 openbox thunar openbox deluge gnumeric abiword rdesktop xchat evince geany viewnior
 exit 0
