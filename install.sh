413639c64aa58e5c7522daac3b58744d8c4d1f9e
Katerberg <katerberg@fastmail.fm>
2015-01-05 20:38:15 -0600

    Office suite

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install tmux conky aptitude git firefox pidgin keepassx claws-mail claws-mail-pgpmime gnupg2 arandr vlc gimp tint2 openbox thunar openbox deluge
+sudo apt-get install tmux conky aptitude git firefox pidgin keepassx claws-mail claws-mail-pgpmime gnupg2 arandr vlc gimp tint2 openbox thunar openbox deluge gnumeric abiword
 exit 0
