34173a9ed43d9761153fced2d1734446e39a57c0
Katerberg <katerberg@fastmail.fm>
2015-01-10 11:20:25 -0600

    Adds consolekit

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install tmux conky aptitude git firefox pidgin keepassx claws-mail claws-mail-pgpmime claws-mail-plugins claws-mail-extra-plugins gnupg2 arandr vlc gimp tint2 openbox thunar openbox deluge gnumeric abiword rdesktop xchat evince geany viewnior volumeicon-alsa
+sudo apt-get install tmux conky aptitude git firefox pidgin keepassx claws-mail claws-mail-pgpmime claws-mail-plugins claws-mail-extra-plugins gnupg2 arandr vlc gimp tint2 openbox thunar openbox deluge gnumeric abiword rdesktop xchat evince geany viewnior volumeicon-alsa consolekit
 exit 0
