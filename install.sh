140f84499a3c1ea65db5a6ef5bb4bf6ff7c6a2ac
Katerberg <katerberg@fastmail.fm>
2015-01-12 17:36:07 -0600

    Adds more to install

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install tmux conky aptitude git firefox pidgin keepassx claws-mail claws-mail-pgpmime claws-mail-plugins claws-mail-extra-plugins gnupg2 arandr vlc gimp tint2 openbox thunar openbox deluge gnumeric abiword rdesktop xchat evince geany viewnior volumeicon-alsa consolekit
+sudo apt-get install tmux conky aptitude git firefox pidgin keepassx claws-mail claws-mail-pgpmime claws-mail-plugins claws-mail-extra-plugins gnupg2 arandr vlc gimp tint2 openbox thunar openbox deluge gnumeric abiword rdesktop xchat evince geany viewnior volumeicon-alsa consolekit htop synaptic npm
 exit 0
